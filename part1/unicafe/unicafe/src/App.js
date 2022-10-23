40e475c7388350c4a18e175a820818df4dc432c8
Roman Geathers <romangeathersdev@gmail.com>
2022-10-23 15:14:19 -0400

    Add average and percent positive feedback displays

--- part1/unicafe/unicafe/src/App.js
+++ part1/unicafe/unicafe/src/App.js
@@ -12,10 +12,10 @@ const Button = ({ text, handleClick}) => {
   )
 }
 
-const FeedbackCount = ({ text, count }) => {
+const FeedbackStat = ({ text, stat }) => {
   return (
     <div>
-      <p>{text} {count}</p>
+      <p>{text} {stat}</p>
     </div>
   )
 }
@@ -24,30 +24,43 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
 
-  const handleGoodClick = () => {
-    setGood(good + 1);
+  const handleFeedbackClick = (e) => {
+    if (e.target.textContent === 'good') {
+      setGood(good + 1);
+    } else if (e.target.textContent === 'neutral') {
+      setNeutral(neutral + 1);
+    } else if (e.target.textContent === 'bad') {
+      setBad(bad + 1);
+    }
+    setAll(good + neutral + bad + 1);
   }
 
-  const handleNeutralClick = () => {
-    setNeutral(neutral + 1);
+  const averageFeedback = () => {
+    if (all === 0) {
+      return 0;
+    } else {
+      return (good - bad) / all;
+    }
   }
 
-  const handleBadClick = () => {
-    setBad(bad + 1);
-  }
+  const positiveFeedback = () => `${(good / all) * 100} %`;
 
   return (
     <div>
       <Header header="give feedback" />
-      <Button text="good" handleClick={handleGoodClick}/>
-      <Button text="neutral" handleClick={handleNeutralClick}/>
-      <Button text="bad" handleClick={handleBadClick}/>
+      <Button text="good" handleClick={handleFeedbackClick}/>
+      <Button text="neutral" handleClick={handleFeedbackClick}/>
+      <Button text="bad" handleClick={handleFeedbackClick}/>
 
       <Header header="statistics" />
-      <FeedbackCount text="good" count={good} />
-      <FeedbackCount text="neutral" count={neutral} />
-      <FeedbackCount text="bad" count={bad} />
+      <FeedbackStat text="good" stat={good} />
+      <FeedbackStat text="neutral" stat={neutral} />
+      <FeedbackStat text="bad" stat={bad} />
+      <FeedbackStat text="all" stat={all} />
+      <FeedbackStat text="average" stat={averageFeedback()} />
+      <FeedbackStat text="positive" stat={positiveFeedback()} />
     </div>
   );
 }
