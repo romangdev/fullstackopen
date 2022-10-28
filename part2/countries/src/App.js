fed5aaeee420bb0cf75cad246ad44858ca64f24c
Roman Geathers <romangeathersdev@gmail.com>
2022-10-27 20:09:57 -0400

    Continue with countries exercise 2.12

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -34,7 +34,25 @@ const App = () => {
         {countryList.length > 10 ?
           'Too many matches, please keep typing'
           :
-          countryList.map(country => <div key={country.car.ccn3}>{country.name.common}</div>)
+          countryList.map(country => <div key={country.name.common}>{country.name.common}</div>)
+        }
+        {countryList.length === 1 ?
+          <div>
+            <h2>{countryList[0].name.common}</h2>
+            <p>Capital: {countryList[0].capital}</p>
+            <p>Area: {countryList[0].area}</p>
+            <h3>Languages</h3>
+            <ul>
+              {Object.values(countryList[0].languages).map(language => {
+                return <li key={language}>{language}</li>
+              })}
+            </ul>
+            <div>
+              <img src={countryList[0].flags.png} alt='Country flag'></img>
+            </div>
+          </div>
+          :
+          <></>
         }
       </div>
     </div>
